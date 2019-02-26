b73c68434d00e10c310e0a8be11906d6aef33f08
remo7777 <28594846+remo7777@users.noreply.github.com>
2019-02-26 20:54:08 +0530

    final update

--- cipher.sh
+++ cipher.sh
@@ -1,5 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
-script_location="/data/data/com.termux/files/usr/share"
+cd /data/data/com.termux/files/usr/share
+script_location="/data/data/com.termux/files/home"
 path_location="/data/data/com.termux/files/usr/bin"
 current_location="/data/data/com.termux/files/home"
 # clone
@@ -7,7 +8,7 @@ apt-get install git -y
 git clone --recursive https://github.com/ash-shell/ash.git
 # Add to /data/data/com.termux/files/usr/bin/applets
 cd "$path_location"
-ln -s "/ash/ash" .
+ln -s "/data/data/com.termux/files/usr/share/ash/ash" .
 echo "Ash successfully installed to /ash"
 # Move back
 chmod 700 /data/data/com.termux/files/usr/bin/ash
